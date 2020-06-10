9fedcb00218eb28f063c8ee98ec4d4262a79d021
prohor33 <prohorgladkikh@gmail.com>
2020-06-10 13:58:58 +0300

    roberta-base-1.12

--- main/run.py
+++ main/run.py
@@ -21,7 +21,7 @@ import os
 
 class Config:
     def __init__(self,
-                 version='roberta-base-1.11',
+                 version='roberta-base-1.12',
                  device='cuda:0',
                  debug=True,
                  eval=False,
